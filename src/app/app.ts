bef3d0323f8d1fb74eed2be7f14dfba427c44f4b
LAPTOP-VBENKGMI\etuwu <etuwuachin@gmail.com>
2026-05-19 08:18:08 -0500

    Se actualizo el app.ts y se eliminaron los archivos spects.ts de la carpeta pages y services

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,19 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecursoComponent } from './pages/recurso/recurso.component';
 import { ProveedorComponent } from './pages/proveedor/proveedor.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { ProductoFinalComponent } from './pages/producto-final/producto-final.component';
+// import { PagoComponent } from './pages/pago/pago.component';
+import { MetodopagoComponent } from './pages/metodopago/metodopago.component';
+
 
 @Component({
   selector: 'app-root',
   imports:[
-    RecursoComponent,
-    ProveedorComponent
+    RecursoComponent, ProveedorComponent,
+    InventarioComponent, ProductoFinalComponent,
+    /* PagoComponent, */ MetodopagoComponent
+
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
