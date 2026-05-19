6ef7cc203f7e24726a82aa7430cd4d9c44a0d497
LAPTOP-HEM6SFEI\PC <oscaralexis1204@gmail.com>
2026-05-19 10:29:35 -0500

    Se actualizo metodopago.component.ts, pago.component.ts

--- src/app/pages/metodopago/metodopago.component.ts
+++ src/app/pages/metodopago/metodopago.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component , inject } from '@angular/core';
+import { MetodoPago } from '../../model/metodopago';
+import { MetodoPagoService } from '../../services/metodopago.service';
+/*Metodo Pago.ts*/
 @Component({
   selector: 'app-metodopago',
   imports: [],
   templateUrl: './metodopago.component.html',
   styleUrl: './metodopago.component.css',
 })
-export class MetodopagoComponent {}
+export class MetodoPagoComponent {
+  protected MetodosPago: MetodoPago[] = []; 
+  private readonly  metodopagoService = inject(MetodoPagoService); 
+
+  ngOnInit () : void {
+
+    this.metodopagoService.findAll().subscribe(data => this.MetodosPago = data); 
+
+  }
+
+}
--- src/app/pages/pago/pago.component.ts
+++ src/app/pages/pago/pago.component.ts
@@ -1,21 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core'; 
-import { CommonModule } from '@angular/common'; 
+import { Component , inject } from '@angular/core';
 import { Pago } from '../../model/pago';
 import { PagoService } from '../../services/pago.service';
-
+/*Pago.ts*/
 @Component({
   selector: 'app-pago',
-  standalone: true,
-  imports: [CommonModule], 
+  imports: [],
   templateUrl: './pago.component.html',
   styleUrl: './pago.component.css',
 })
-export class ProveedorComponent implements OnInit { 
-  protected pagos: Pago[] = [];
-  private readonly pagoService = inject(PagoService);
+export class PagoComponent {
+  protected Pagos: Pago[] = []; 
+  private readonly  pagoService = inject(PagoService); 
 
-  ngOnInit(): void {
-    this.pagoService.findAll().subscribe(data => this.pagos = data);
+  ngOnInit () : void {
+
+    this.pagoService.findAll().subscribe(data => this.Pagos = data); 
+
   }
+
 }
 
