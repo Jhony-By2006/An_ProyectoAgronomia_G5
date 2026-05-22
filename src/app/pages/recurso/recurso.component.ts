8b89aa8be5d1ccaa2c01c691153cbc8995eed9e7
LAPTOP-VBENKGMI\etuwu <etuwuachin@gmail.com>
2026-05-21 21:37:44 -0500

    Se agrego el component de layout, además se uso utilizo Angular Material Data Table para que nuestros datos creadas de nuetra api sean visualisadas, además se actualizo el codigo de proveedor y recurso

--- src/app/pages/recurso/recurso.component.ts
+++ src/app/pages/recurso/recurso.component.ts
@@ -1,21 +1,44 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { Recurso } from '../../model/recurso';
 import { RecursoService } from '../../services/recurso.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recurso',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './recurso.component.html',
   styleUrl: './recurso.component.css',
 })
-export class RecursoComponent {
+export class RecursoComponent implements OnInit {
+  
+  // Almacena los datos de la tabla de forma reactiva
+  protected $dataSource = signal(new MatTableDataSource<Recurso>());
+  
+  // Define los nombres y el orden de las columnas que se renderizarán en el HTML
+  protected displayedColumns: string[] = ['idRecurso', 'nombreRecurso', 'tipoRecurso', 'cantidadRecurso', 'costoRecurso', 'estadoRecurso'];
 
-  protected recursos: Recurso[] = [];
-
+  // Inyección del servicio para consumir la API
   private readonly recursoService = inject(RecursoService);
 
   ngOnInit(): void {
-    // this.examService.findAll().subscribe(data => console.log(data));
-    this.recursoService.findAll().subscribe(data => this.recursos = data);
+    // Obtiene los recursos del backend y los asigna a la tabla
+    this.recursoService.findAll().subscribe(data => {
+      this.$dataSource.set(new MatTableDataSource<Recurso>(data));
+    });
   }
 }
