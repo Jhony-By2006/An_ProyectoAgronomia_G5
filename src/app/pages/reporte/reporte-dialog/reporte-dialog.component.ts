29c8a49b7274f050be36797e475025c8758ba3b2
RenatoCortez <chupetesempai@gmail.com>
2026-05-24 18:04:32 -0500

    fix(reporte):correcion de reporte-dialog fix(trabajador):correcion de trabajador-edit fix(service):correcion det trabajdor.service

--- src/app/pages/reporte/reporte-dialog/reporte-dialog.component.ts
+++ src/app/pages/reporte/reporte-dialog/reporte-dialog.component.ts
@@ -1,30 +1,56 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MAT_DIALOG_DATA,
-  MatDialogActions,
-  MatDialogContent,
-  MatDialogTitle,
-  MatDialogRef
-} from '@angular/material/dialog';
-import { Reporte } from '../../../model/reporte';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReporteService } from '../../../services/reporte.service';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-reporte-dialog',
   imports: [
-    MatDialogTitle,
-    MatDialogContent,
-    MatDialogActions,
-    MatButtonModule
+    MatDialogModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
   ],
   templateUrl: './reporte-dialog.component.html',
   styleUrl: './reporte-dialog.component.css',
 })
 export class ReporteDialogComponent {
 
-  protected reporte: Reporte = inject(MAT_DIALOG_DATA);
-
+  private readonly reporteService = inject(ReporteService);
+  private readonly data = inject(MAT_DIALOG_DATA);
   private readonly dialogRef = inject(MatDialogRef<ReporteDialogComponent>);
+
+  protected $reporte = signal({ ... this.data });
+
+  operate(){
+
+    const reporte = this.$reporte();
+
+    const isEdit = reporte != null && reporte.idReporte > 0;
+
+    const msg = isEdit ? 'UPDATED' : 'CREATED';
+
+    const operation$ = isEdit
+      ? this.reporteService.update(reporte.idReporte, reporte)
+      : this.reporteService.save(reporte);
+
+    operation$.pipe(
+      switchMap(() => this.reporteService.findAll()),
+      tap(data => this.reporteService.setListChange(data)),
+      tap(() => this.reporteService.setMessageChange(msg))
+    )
+    .subscribe(() => this.close());
+
+  }
 
   close(){
     this.dialogRef.close();
