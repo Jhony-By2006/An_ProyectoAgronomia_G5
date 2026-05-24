f630fd696ccfbe41c45fa137d504db57589967d3
LAPTOP-VBENKGMI\etuwu <etuwuachin@gmail.com>
2026-05-23 22:16:28 -0500

    Se actualizo el app.routes, layout y el proveedor y recurso, se mejoraro el dise;o del html y el css, ademas se agrego el logo del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,23 @@ import { Routes } from '@angular/router';
 import { RecursoComponent } from './pages/recurso/recurso.component';
 import { ProveedorComponent } from './pages/proveedor/proveedor.component';
 import { ProveedorEditComponent } from './pages/proveedor/proveedor-edit/proveedor-edit.component';
+import { RecursoEditComponent } from './pages/recurso/recurso-edit/recurso-edit.component';
 
 export const routes: Routes = [
- { 
-        path: 'pages/proveedor', component: ProveedorComponent,
-        children: [
-            { path: 'new', component: ProveedorEditComponent },
-            { path: 'edit/:id', component: ProveedorEditComponent },
-        ],
-    },
+  { 
+    path: 'pages/proveedor', 
+    component: ProveedorComponent,
+    children: [
+      { path: 'new', component: ProveedorEditComponent },
+      { path: 'edit/:id', component: ProveedorEditComponent },
+    ],
+  },
+  { 
+    path: 'pages/recurso', 
+    component: RecursoComponent,
+    children: [
+      { path: 'new', component: RecursoEditComponent },
+      { path: 'edit/:id', component: RecursoEditComponent },
+    ],
+  },
 ];
